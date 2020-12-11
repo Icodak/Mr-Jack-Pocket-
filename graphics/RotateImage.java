0d6ede71449bfc1998323dca0e0d63eb7aae2fd4
Icodak <24356594+Icodak@users.noreply.github.com>
2020-12-11 10:04:35 +0100

    graphic

---
+++ graphics/RotateImage.java
@@ -0,0 +1,17 @@
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+public class RotateImage {
+	
+	static ImageIcon rotateImage() {	
+	
+	}
+}
